1bc16098fadd1d123543f2fb4c5c67bd8e7794d0
Olaniyi Oshunbote <oshunboteo@gmail.com>
2019-09-25 17:24:29 +0100

    moving...

--- calculator.js
+++ calculator.js
@@ -2,25 +2,33 @@ console.log("We are here.");
 
 total_appliances = [
     {
-        appliance: "",
         quantity: "",
         wattage: "",
         time: ""
     }
 ];
+// let input_result = form.appliance;
 
-
-function clearAll() {
-
+function more_input() {
+    let add_input = document.querySelector("#add_input");
+    let add_content = add_input.content;
+    document.querySelector("#inputs").appendChild(add_content);
+    // console.log("we cool?");
+    // let input = document.querySelector("#add_input");
+    // document.querySelector("inputs").append(input);
 }
 
-function solar_calculator(form) {
-    console.log(total_appliances[0].appliance);
-    for (i = 0; i < total_appliances.length; i++) {
-        qty = form.amount.value;
-        hrs = form.hour.value;
-        wattage = form.kWh.value;
-        times = qty * hrs;
-        console.log(times);
-    };
-}
+// function clearAll() {
+
+// }
+
+// function solar_calculator() {
+//     console.log(total_appliances[0].appliance);
+//     for (i = 0; i < total_appliances.length; i++) {
+//         qty = form.amount.value;
+//         hrs = form.hour.value;
+//         wattage = form.kWh.value;
+//         times = qty * hrs;
+//         console.log(times);
+//     };
+// }
