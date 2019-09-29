9784bb22cb29a63b9d004d7932d39c6e6e1d1764
Erons <eronmmer@gmail.com>
2019-09-29 15:39:57 +0100

    fixed bugs

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     );
   });
 
-  let count = 2;
+  let count = 3;
   let total = 0;
   let report,
     details,
@@ -39,17 +39,21 @@ $(document).ready(function() {
     hourlyEnergyRequired,
     product;
   // parseFloat($(this).val()).toFixed(2)
-
+  report = false;
   let obj = [];
-
+  
   let ratings = {
     "LED Light Bulb": 10,
     "CFL Light Bulb": 14,
-    "Incandescent Bulb": 60,
+    "60W Incandescent Bulb": 60,
+    "100W Incandescent Bulb": 100,
     "LCD/LED TV/DISPLAY": 90,
-    Plasma: 150,
+    "Towel Heater" : 150,
+    "Electric Shaver" : 15,
+    Plasma: 300,
     "CRT MONITOR": 120,
     "Game Console": 200,
+    "Gaming PC": 600,
     "Desktop(minus display)": 300,
     Laptop: 60,
     "Wi-Fi Router": 6,
@@ -75,9 +79,37 @@ $(document).ready(function() {
     Toaster: 1200,
     "Hair Dryer": 1500,
     Iron: 1100,
+    "Steam Iron" : 2500,
     Vacuum: 1400,
-    "Ceiling Fan": 75
+    "Ceiling Fan": 75,
+    "Electric Kettle": 2000,
+    "Electric Mower": 1500,
+    "Standing Fan": 60,
+    "DVD Player" : 60,
+    "Projector" : 270,
+    "Shredder": "220",
+    "Home Sound System" : 100,
+    "Pressure Cooker" : 700,
+    "Rice Cooker" : 400,
+    "Scanner" : 30,
+    "Steriliser" : 650,
+    "Table Fan" : 25,
+    "Treadmill" : 1500,
+    "Wall Fan" : 55,
+    "Water Dispenser" : 100,
+    "Water Filter" : 100,
+    "Blender" : 800,
+    "Fryer" : 1000,
+    "Power Shower" : 9000,
+    "Sewing Machine" : 80,
+    "Electric Drill" : 850,
+    "Fluorescnt tube" : 82,
+    "Security Light" : 1000,
+    "IP Camera" : 15,
+    "Hand Mixer" : 250,
+    "Amplifier" : 1000
   };
+
   let states = {
     Abia: 5,
     Adamawa: 9,
@@ -133,8 +165,8 @@ $(document).ready(function() {
     //            <td> <input  id="daily-${count}" class="daily" value="0"></td>
     //             <td><button class="remove-row">X</button></td>
     // </tr></div>`;
-
-     let addRow = formTable.insertRow(4);
+      console.log(count)
+     let addRow = formTable.insertRow(count);
      addRow.innerHTML = `<tr class="removeMe" id="pleaseRemoveMe">
     <td><select id="appliance-${count}" class="appliances" required>
                   <option value="" selected hidden>Select</option>
@@ -142,6 +174,7 @@ $(document).ready(function() {
                 <td><input id="qty-${count}" class="qty" value="1" min="0" max="999" type="number"></td>
                 <td><input id="rating-${count}" value=0 class="watts" type=text disabled/></td>
                 <td><input id="hours-${count}" class="hours" min="0" max="24" step="1" value="0" type="number"></td>
+                <td> <input  id="daily-${count}" class="daily" value="0"></td>
                 <td><button class="remove-row">X</button></td>
     </tr>`;
 
@@ -164,17 +197,17 @@ $(document).ready(function() {
     let offer = "";
     switch (true) {
       case hourlyEnergyRequired < 1000:
-        offer = "Muses 1kw Flexible Monocrystalline Solar Panels";
+        offer = "Muses 1kwh Flexible Monocrystalline Solar Panels";
         console.log("I am here 1");
         break;
       case hourlyEnergyRequired < 5000:
-        offer = "Muses 2-5kw Foldable Solar Panels";
+        offer = "Muses 2-5kwh Foldable Solar Panels";
         break;
       case hourlyEnergyRequired < 7000:
-        offer = "Muses 7kw Polycrystalline High Efficient Solar Panels";
+        offer = "Muses 7kwh Polycrystalline High Efficient Solar Panels";
         break;
       case hourlyEnergyRequired < 10000:
-        offer = "Muses 10kw Polycrystalline Solar Panels";
+        offer = "Muses 10kwh Polycrystalline Solar Panels";
         break;
       default:
         offer = "Muses Industrial Workhorse Polycrystalline Solar Panels";
@@ -221,65 +254,72 @@ $(document).ready(function() {
   });
 
   $("#add-Row").on("click", function() {
-    console.log("here");
-    createRow(ratings);
+    if(!report){
+      createRow(ratings);
+      console.log("here");
+      }
   });
 
   $("#worksheet").on("click", ".remove-row", function() {
     $(this)
       .closest("tr")
       .remove();
+      count--
   });
 
   document.querySelectorAll(".showResult").forEach(elem => {
     elem.addEventListener("click", function() {
-      if (residence !== undefined) {
-        $(".appliances").each(function(index, value) {
-          obj[index] = {};
-          obj[index].appliances = $(this).val();
-        });
-
-        $(".qty").each(function(index, value) {
-          obj[index].quantity = $(this).val();
-        });
-
-        $(".watts").each(function(index, value) {
-          obj[index].rating = $(this).val();
-        });
-
-        $(".hours").each(function(index, value) {
-          obj[index].hours = $(this).val();
-        });
-
-        $(".daily").each(function(index, value) {
-          obj[index].consumptionPerDevice = $(this).val();
-          console.log(obj);
-          console.log($(this));
-          total += +$(this).val();
-          hourlyEnergyRequired = (total / sunHours).toFixed(2);
-          panels = Math.ceil(hourlyEnergyRequired / 320);
-          console.log(total);
-        });
-        product = recommendProduct(hourlyEnergyRequired);
-        details = `Dear <b>${name}</b>,
-              Your daily energy need is <strong>${total}</strong> wattshr. The average sun-hours in <b>${residence}</b> is <b>${sunHours}</b>hours. Hence, you will need <b>${panels}</b> solar panel(s) to provide an average of <b>${hourlyEnergyRequired}</b> watts per sun-hour. Armed with this information, we would like to recommend our <b>${product}</b>.`;
-
-        $("#analysis").html(details);
-        createDataArrays();
-        $("#power-needed").html(
-          '<button style="padding: 0.5rem 1rem; background: green; border-radius: 20px; outline: none;"id="viewChart" type="button">Click To View Chart</button>'
-        );
-      } else {
-        console.log("fill");
-        $("#guide").html(
-          '<p style="color:red; font-weight:bold; font-size:1.5em;">Please enter your location</p>'
-        );
-        setTimeout(() => {
-          $("#guide").html(
-            '<p style="font-weight: bold" id="guide"><span style="color:red; font-size:1.5em;">*</span>Select your state of residence</p>'
+      if(!report){
+        if (residence !== undefined) {
+          $(".appliances").each(function(index, value) {
+            obj[index] = {};
+            obj[index].appliances = $(this).val();
+          });
+  
+          $(".qty").each(function(index, value) {
+            obj[index].quantity = $(this).val();
+          });
+  
+          $(".watts").each(function(index, value) {
+            obj[index].rating = $(this).val();
+          });
+  
+          $(".hours").each(function(index, value) {
+            obj[index].hours = $(this).val();
+          });
+  
+          $(".daily").each(function(index, value) {
+            obj[index].consumptionPerDevice = $(this).val();
+            console.log(obj);
+            console.log($(this));
+            total += +$(this).val() * 1.25;
+            hourlyEnergyRequired = (total / sunHours).toFixed(2);
+            panels = Math.ceil(hourlyEnergyRequired / 320);
+            console.log(total);
+          });
+          product = recommendProduct(hourlyEnergyRequired);
+          details = `Dear <b>${name}</b>,
+                your daily energy need is <strong>${total}</strong> watts-hour (+25% tolerance). <b>${residence}</b> has an average of <b>${sunHours}</b> sun-hours. Hence, you will need <b>${panels}</b> solar panel(s) to provide an average of <b>${hourlyEnergyRequired}</b> watts per sun-hour. Armed with this information, we would like to recommend our <b>${product}</b>.`;
+  
+          $("#analysis").html(details);
+          createDataArrays();
+          $("#power-needed").html(
+            '<button style="padding: 0.5rem 1rem; background: green; border-radius: 20px; outline: none; color:white;"id="viewChart" type="button">Click To View Chart</button>'
           );
-        }, 3000);
+          report = true;
+        } else {
+          
+          $("#guide").html(
+            '<p style="color:red; font-weight:bold; font-size:1em;">Please enter your location</p>'
+          );
+          setTimeout(() => {
+            $("#guide").html(
+              '<p style="font-weight: bold" id="guide"><span style="color:red; font-size:1.2rem;">*</span>Select your state of residence</p>'
+            );
+          }, 3000);
+        }
       }
+      
     });
   });
 
@@ -390,7 +430,7 @@ $(document).ready(function() {
     $("#charts").html("");
     $("#charts").hide();
     $("#power-needed").html("TOTAL POWER NEEDED");
-
+    report = false;
     const entries = document.querySelectorAll(".removeMe");
     // entries.forEach(entry => {
     //   if ( entry.getAttribute("id") ===  "removeTest"  ) {
@@ -444,7 +484,7 @@ $(document).ready(function() {
 
   function createCharts(element, choice, xArray, yArray, title) {
     var ctx = document.getElementById(element).getContext("2d");
-    console.log(choice, xArray, yArray);
+    // console.log(choice, xArray, yArray);
     var myChart = new Chart(ctx, {
       type: choice,
       type: choice,
